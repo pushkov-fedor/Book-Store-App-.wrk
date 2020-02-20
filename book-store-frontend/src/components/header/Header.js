c9c186dc87e5749ef9d40946c7fb73c3d0c4e494
Fedor <f.pushkov@g.nsu.ru>
2020-02-20 18:25:37 +0700

    Trying to fix wrong navbar styling on Safari

--- book-store-frontend/src/components/header/Header.js
+++ book-store-frontend/src/components/header/Header.js
@@ -6,7 +6,7 @@ import "./Header.css";
 function Header(props) {
 
     return (
-        <nav className="navbar navbar-expand-sm position-sticky fixed-top navbar-container">
+        <nav className="navbar navbar-expand-sm fixed-top navbar-container">
             <div className="container flex-column flex-sm-row">
                 <Link className="navbar-brand text-white mr-0 justify-content-center justify-content-xs-start navbar-brand-custom" to="/">My Book Store</Link>
                 <ul className="nav justify-content-center justify-content-xs-end">

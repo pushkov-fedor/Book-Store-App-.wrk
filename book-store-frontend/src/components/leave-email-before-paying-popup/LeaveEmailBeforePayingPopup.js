04119aa5617ebe9ef2a7c76d4f72514fa4d7c302
Fedor <f.pushkov@g.nsu.ru>
2020-02-25 15:37:00 +0700

    Added another div in LeaveEmailBeforePayingPopup.js cause of Safari rgba() opacity problem

--- book-store-frontend/src/components/leave-email-before-paying-popup/LeaveEmailBeforePayingPopup.js
+++ book-store-frontend/src/components/leave-email-before-paying-popup/LeaveEmailBeforePayingPopup.js
@@ -29,25 +29,29 @@ const LeaveEmailBeforePayingPopup = inject("rootStore")(observer(props => {
     }
 
     return (
+      <div className="">
         <div className="position-fixed d-flex justify-content-center align-items-center leave-email-before-paying-popup-container"
              id="popup-bg" onClick={handleClickOnBg}>
-            <div className="m-3 p-5 bg-white leave-email-before-paying-popup-subcontainer">
-                <h3 className="leave-email-before-paying-popup-last-moment-header">One last moment...</h3>
-                <h5 className="leave-email-before-paying-popup-email-header">Before paying leave your email so that we could send you your books</h5>
-                <div className="row mt-3 mt-sm-4 d-flex justify-content-center align-items-center mx-0">
-                    <div className="input-group col-12 col-sm-6 px-0 mb-2 mb-sm-0">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text">@</div>
-                        </div>
-                        <input type="email" className="form-control" id="inlineFormInputGroup"
-                               placeholder="Email" value={customerEmail.get()} onChange={handleChange}/>
-                    </div>
-                    <Link to={{pathname: "/after-paying", state: {customerEmail: customerEmail.get()}}} onClick={() => sendEmail()}
-                          style={{pointerEvents: customerEmail.get() === "" ? "none" : "auto"}}
-                          className={`btn ${customerEmail.get() === "" ? "btn-secondary" : "btn-primary"} w-100 col-12 col-sm-3 mx-3`}>Submit</Link>
-                </div>
-            </div>
         </div>
+          <div className="m-3 leave-email-before-paying-popup-subcontainer m-sm-5">
+              <div className="m-0 bg-white p-5 mx-sm-5 leave-email-before-paying-popup-subsubcontainer">
+                  <h3 className="leave-email-before-paying-popup-last-moment-header">One last moment...</h3>
+                  <h5 className="leave-email-before-paying-popup-email-header">Before paying leave your email so that we could send you your books</h5>
+                  <div className="row mt-3 d-flex justify-content-between align-items-center mx-0">
+                      <div className="input-group col-12 col-md-7 col-xl-7 px-0 mb-2 mb-md-0">
+                          <div className="input-group-prepend">
+                              <div className="input-group-text">@</div>
+                          </div>
+                          <input type="email" className="form-control" id="inlineFormInputGroup"
+                                 placeholder="Email" value={customerEmail.get()} onChange={handleChange}/>
+                      </div>
+                      <Link to={{pathname: "/after-paying", state: {customerEmail: customerEmail.get()}}} onClick={() => sendEmail()}
+                            style={{pointerEvents: customerEmail.get() === "" ? "none" : "auto"}}
+                            className={`btn ${customerEmail.get() === "" ? "btn-secondary" : "btn-primary"} w-100 col-12 col-md-4 col-xl-3 mx-1 mx-xl-3`}>Submit</Link>
+                  </div>
+              </div>
+          </div>
+      </div>
     );
 }))
 

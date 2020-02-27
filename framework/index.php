8b30275fe083fdb72a5b97fb9d83ac95c7202c02
Fedor <f.pushkov@g.nsu.ru>
2020-02-27 18:10:06 +0700

    Added errors reporting in php

--- framework/index.php
+++ framework/index.php
@@ -6,6 +6,10 @@ spl_autoload_register(function($class){
 
 use router\Router;
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $router = new Router();
 include_once "config/routerConfig.php";
 
